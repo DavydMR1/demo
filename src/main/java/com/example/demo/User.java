02763b10afd7e91ec7716ba5b522ab72e091031b
davyd <allbuddyes@gmail.com>
2024-10-01 07:57:22 +0300

    a

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import java.util.Objects;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public User() {
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        User user = (User) object;
+        return id == user.id && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, id);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
